727efc1e20955607d629d3f5a88c35f538420d2e
phricardorj <brpedroricardo@gmail.com>
2022-03-04 11:09:36 -0300

    Removed Database model

--- src/main/java/br/com/letscode/StarWarsAPI/model/Rebelde.java
+++ src/main/java/br/com/letscode/StarWarsAPI/model/Rebelde.java
@@ -4,6 +4,8 @@ package br.com.letscode.StarWarsAPI.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter @Setter
@@ -15,6 +17,7 @@ public class Rebelde {
     private Localizacao localizacao;
     private Inventario inventario;
     private boolean Traidor = false;
+    private static List<Rebelde> rebeldes = new ArrayList<>();
 
     public Rebelde(UUID id, String nome, int idade, Genero genero, Localizacao localizacao, Inventario inventario){
         this.id = id;
@@ -23,6 +26,14 @@ public class Rebelde {
         this.genero = genero;
         this.localizacao = localizacao;
         this.inventario = inventario;
+    }
+
+    public static void adiciona(Rebelde rebelde){
+        Rebelde.rebeldes.add(rebelde);
+    }
+
+    public static List<Rebelde> getRebeldes() {
+        return rebeldes;
     }
 
 }
