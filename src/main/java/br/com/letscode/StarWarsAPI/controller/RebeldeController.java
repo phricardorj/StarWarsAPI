35d095898abc42b3c9a0947416adf6690e7570b8
israelcena <israelsjm@gmail.com>
2022-03-05 14:56:16 -0300

    include list rebel and update rebel loc

--- src/main/java/br/com/letscode/StarWarsAPI/controller/RebeldeController.java
+++ src/main/java/br/com/letscode/StarWarsAPI/controller/RebeldeController.java
@@ -1,8 +1,10 @@
 package br.com.letscode.StarWarsAPI.controller;
 
 import br.com.letscode.StarWarsAPI.dto.RequestRebelde;
+import br.com.letscode.StarWarsAPI.model.Localizacao;
 import br.com.letscode.StarWarsAPI.model.Rebelde;
 import br.com.letscode.StarWarsAPI.service.RebeldeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +20,21 @@ public class RebeldeController {
     public List<Rebelde> listaRebeldes(){
         return Rebelde.getRebeldes();
     }
+
+    @GetMapping("/listarloc/{id}")
+    public Localizacao listarLoc(@PathVariable UUID id) {
+        return listaRebeldes().stream().filter(rebelde -> rebelde.getId().equals(id)).findFirst().get().getLocalizacao();
+    }
+
+    @PatchMapping("/atualizar/{id}")@ResponseStatus(HttpStatus.CREATED)
+    public Localizacao listarLoc(@PathVariable UUID id, @RequestBody Localizacao localizacao){
+        Localizacao rebelLoc = listaRebeldes().stream().filter(rebelde -> rebelde.getId().equals(id)).findFirst().get().getLocalizacao();
+        rebelLoc.setLatitude(localizacao.getLatitude());
+        rebelLoc.setLongitude(localizacao.getLongitude());
+        rebelLoc.setNome(localizacao.getNome());
+        return rebelLoc;
+    }
+
 
     @PostMapping
     public Rebelde cadastrar(@RequestBody @Valid RequestRebelde form){
