99032c275491dbbf9dc0c6116339ff36d5525e44
israelcena <israelsjm@gmail.com>
2022-03-04 22:43:20 -0300

    create report dto

---
+++ src/main/java/br/com/letscode/StarWarsAPI/dto/ReportRebelde.java
@@ -0,0 +1,13 @@
+package br.com.letscode.StarWarsAPI.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReportRebelde {
+  private String nome;
+  private double latitude;
+  private double logintude;
+  private String nomeGalaxia;
+}
